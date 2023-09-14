31f66b4cb0f261289720996d7b64efa416a7aac7
elfedyukova <elfedyukova@yandex.ru>
2023-09-14 22:37:05 +0300

    add sprint 6

--- src/main/java/org/example/managers/file/CSVFormatHandler.java
+++ src/main/java/org/example/managers/file/CSVFormatHandler.java
@@ -8,14 +8,11 @@ import org.example.task.TaskType;
 import java.util.ArrayList;
 import java.util.List;
 
-//для сохранения и восстановления менеджера истории из CSV.
-
 public class CSVFormatHandler {
 
     private static final String DELIMITER = ",";
 
     String toString(Task task) {
-        //1,TASK,Task1,NEW,Description task1, на выходе должны получить
         String result = task.getId() + DELIMITER +
                 task.getType() + DELIMITER +
                 task.getName() + DELIMITER +
@@ -23,32 +20,25 @@ public class CSVFormatHandler {
                 task.getDescription() + DELIMITER;
 
         if (task.getType() == TaskType.SUBTASK) {
-            result = result + ((Subtask) task).getEpicId();//конвертируем таску в сабтаск
+            result = result + ((Subtask) task).getEpicId();
         }
         return result;
     }
 
     Task fromString(String value) {
-//Напишите метод создания задачи из строки Task fromString(String value).
         String id = "";
         String taskType = "";
-        //по второму параметру определяем тип задач
         TaskType type = TaskType.valueOf(taskType);
-        //создаем новый объект через конструктор
         Task task = new Task("task", "Создание задачи");
-
         return task;
     }
 
     String historyToString(HistoryManager manager) {
-        //получаем данные связного  списка истории + проходимся и в каждой задаче получайем id  + джойним через стрингджойн
-        // каждая строка это идентификатор
         List<String> result = new ArrayList<>();
 
         for (Task task : manager.getHistory()) {
             result.add(String.valueOf(task.getId()));
         }
-
         return String.join(DELIMITER, result);
     }
 
